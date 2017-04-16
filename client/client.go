2d50908c0fc7268c01269300aa5132b247b3a6e7
wall <mystaticprivateemail@gmail.com>
2017-04-16 15:49:32 -0600

    server can echo clients request

--- client/client.go
+++ client/client.go
@@ -1,8 +1,38 @@
 package main
 
 import (
-
+	"os"
+	"net/http"
+	"io/ioutil"
+	"bytes"
+	"fmt"
 )
 
 func main() {
+	arg := os.Args[1]
+	switch arg {
+	case "echo":
+		_, err := Echo(os.Args[2])
+		if err == nil {
+			fmt.Println("Got back echo")
+		} else {
+			fmt.Println("Failed to get back echo")
+		}
+	}
+
+
+}
+
+func Echo(msg string) (string, error) {
+	resp, err := http.Post("http://localhost:8080/echo", "text/plain", bytes.NewBufferString(msg))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close();
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	} else {
+		return string(body), nil
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,8 +1,24 @@
 package main
 
 import (
-
+	"log"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
+	http.Handle("/echo", http.HandlerFunc(EchoHandler))
+	fmt.Println("Starting the server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func EchoHandler(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err == nil {
+		fmt.Fprintf(w, string(body))
+		fmt.Println("Responding with echo", string(body))
+	} else {
+		http.Error(w, "Failed to send echo", http.StatusInternalServerError)
+	}
 }
